4bd4851f9c9888984c0df115a112e5ab097e2513
Ashikaga West <AshikagaWest@gmail.com>
2019-08-17 07:36:13 -0700

    Removed checkboxes.  Green background for completed items.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './App.css';
 import * as serviceWorker from './serviceWorker';
 
 const primaryWeapons = require('./primary_build_info.json');
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+/**
+ * Properties:
+ *  storageKey
+ *  text
+ */
+class ClickableItemCell extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.onClickCell = this.onClickCell.bind(this);
+
+        this.state = { checked: false };
+    }
+
+    componentDidMount() {
+        var stgValue = localStorage.getItem(this.props.storageKey);
+
+        var newCheckedState = false;
+        if (stgValue) {
+            newCheckedState = JSON.parse(stgValue);
+        }
+
+        this.setState({ checked : newCheckedState });
+
+        if (this.props.onCheckedUpdate) {
+            this.props.onCheckedUpdate(newCheckedState);
+        }
+    }
+
+    updateStateFromCheckBox(checkbox) {
+        var checked = checkbox.checked;
+        this.setState({ checked: checked });
+
+        localStorage.setItem(this.props.storageKey, JSON.stringify(checked));
+    }
+
+    onClickCell(event) {
+        var newCheckedState = ! this.state.checked;
+        this.setState({ checked: newCheckedState });
+
+        if (this.props.onCheckedUpdate) {
+            this.props.onCheckedUpdate(newCheckedState);
+        }
+
+        localStorage.setItem(this.props.storageKey, JSON.stringify(newCheckedState));
+    }
+
+    render() {
+        var completionClass;
+        if (this.state.checked) {
+            completionClass = "completed-item";
+        } else {
+            completionClass = "incomplete-item";
+        }
+
+        return <td key={this.props.partKey} className={completionClass} onClick={this.onClickCell}>
+            <label>
+                {this.props.text}
+            </label>
+            </td>
+            ;
+    }
+}
 
 // <PrimaryWeaponPartTracker weaponInfo={weaponInfo} partName={partName}/>
 class PrimaryWeaponPartTracker extends React.Component {
@@ -19,60 +81,16 @@ class PrimaryWeaponPartTracker extends React.Component {
         super(props);
 
         this.formatPartLocalStorageKey = this.formatPartLocalStorageKey.bind(this);
-        this.onCheckboxUpdated = this.onCheckboxUpdated.bind(this);
-        this.onClickCell = this.onClickCell.bind(this);
-
-        this.state = { checked: false };
     }
 
     formatPartLocalStorageKey() {
         return "wf-mr-build-parts-tracker//" + this.props.weaponInfo.weaponName + "|" + this.props.partName;
     }
 
-    componentDidMount() {
-        var partStgKey = this.formatPartLocalStorageKey();
-        var stgValue = localStorage.getItem(partStgKey);
-
-        var checked = false;
-        if (stgValue) {
-            checked = JSON.parse(stgValue);
-        }
-        //console.info("read key " + partStgKey + " = " + stgValue);
-
-        if (checked) {
-            this.setState({checked: true});
-        } else {
-            this.setState({checked: false});
-        }
-    }
-
-    onCheckboxUpdated(event) {
-        var checked = event.target.checked;
-        this.setState({ checked: checked });
-
-        var partStgKey = this.formatPartLocalStorageKey();
-        localStorage.setItem(partStgKey, checked);
-        //console.info("set key " + partStgKey + " = " + JSON.stringify(checked));
-    }
-
-    onClickCell(event) {
-        event.stopPropagation();
-
-        var ckboxId = "ck-" + this.props.weaponInfo.weaponName + "|" + this.props.partName;
-        var ckbox = document.getElementById(ckboxId);
-        ckbox.checked = (! ckbox.checked );
-    }
-
     render() {
-        var ckboxId = "ck-" + this.props.weaponInfo.weaponName + "|" + this.props.partName;
+        var partStgKey = this.formatPartLocalStorageKey();
 
-        return <td key={this.props.partKey} onClick={this.onClickCell}>
-            <label>
-                <input type="checkbox" id={ckboxId} checked={this.state.checked} onChange={this.onCheckboxUpdated}/>
-                {this.props.partName}
-            </label>
-            </td>
-            ;
+        return  <ClickableItemCell storageKey={partStgKey} text={this.props.partName}/>
     }
 }
 
@@ -82,8 +100,7 @@ class PrimaryWeaponTracker extends React.Component {
 
         this.renderOnePart = this.renderOnePart.bind(this);
         this.formatWeaponLocalStorageKey = this.formatWeaponLocalStorageKey.bind(this);
-        this.onCheckboxUpdated = this.onCheckboxUpdated.bind(this);
-        this.onClickCell = this.onClickCell.bind(this);
+        this.onCheckedUpdate = this.onCheckedUpdate.bind(this);
 
         this.state = { checked: false };
     }
@@ -97,54 +114,25 @@ class PrimaryWeaponTracker extends React.Component {
         return <PrimaryWeaponPartTracker weaponInfo={this.props.weaponInfo} partName={partName} key={partKey} partKey={partKey}/>
     }
 
-    componentDidMount() {
-        var weaponStgKey = this.formatWeaponLocalStorageKey();
-        var stgValue = localStorage.getItem(weaponStgKey);
-
-        var checked = false;
-        if (stgValue) {
-            checked = JSON.parse(stgValue);
-        }
-        //console.info("read key " + partStgKey + " = " + stgValue);
-
-        if (checked) {
-            this.setState({checked: true});
-        } else {
-            this.setState({checked: false});
-        }
-    }
-
-    onCheckboxUpdated(event) {
-        var checked = event.target.checked;
-        this.setState({ checked: checked });
-
-        var weaponStgKey = this.formatWeaponLocalStorageKey();
-        localStorage.setItem(weaponStgKey, checked);
-        console.info("set key " + weaponStgKey + " = " + JSON.stringify(checked));
-    }
-
-    onClickCell(event) {
-        event.stopPropagation();
-
-        var ckboxId = "ck-" + this.props.weaponInfo.weaponName;
-        var ckbox = document.getElementById(ckboxId);
-        ckbox.checked = (! ckbox.checked );
+    onCheckedUpdate(newCheckedState) {
+        this.setState({ checked: newCheckedState });
     }
 
     render() {
         var rows = this.props.weaponInfo.parts.map(this.renderOnePart);
 
-        var ckboxId = "ck-" + this.props.weaponInfo.weaponName;
+        var completionClass;
+        if (this.state.checked) {
+            completionClass = "completed-item";
+        } else {
+            completionClass = "incomplete-item";
+        }
+
+        var storageKey = this.formatWeaponLocalStorageKey();
 
         return <tr>
-            <td onClick={this.onClickCell} style={{border: "1px solid black"}}>
-            <label>
-                <input type="checkbox" id={ckboxId} checked={this.state.checked} onChange={this.onCheckboxUpdated}/>
-                {this.props.weaponInfo.weaponName}
-                <span/>
-            </label>
-            </td>
-            <td>
+            <ClickableItemCell storageKey={storageKey} text={this.props.weaponInfo.weaponName} onCheckedUpdate={this.onCheckedUpdate}/>
+            <td className={completionClass}>
                 {this.props.weaponInfo.acquisition}
             </td>
             {rows}
@@ -175,8 +163,6 @@ class PrimaryWeaponListTracker extends React.Component {
             </table>
     }
 }
-
-//console.info(primaryWeapons[0].weaponName);
 
 ReactDOM.render(
     <PrimaryWeaponListTracker weaponInfo={primaryWeapons} />,
