ebe64f94bf6fbb84d04750d766cc62abbadc8f66
Ashikaga West <AshikagaWest@gmail.com>
2019-08-22 13:37:06 -0700

    Render the drop info as spans of per-part details instead of raw json.

--- src/components/FarmingInfoViewer.js
+++ src/components/FarmingInfoViewer.js
@@ -7,10 +7,31 @@ export default class FarmingInfoViewer extends React.Component {
 
         this.state = { partDrops: this.props.farmingStore.getPartDrops() };
 
-        this.props.farmingStore.addListener(() => this.setState({ partDrops: this.props.farmingStore.getPartDrops()}))
+        this.props.farmingStore.addListener(() => this.setState({ partDrops: this.props.farmingStore.getPartDrops()}));
+
+        this.renderOneDrop = this.renderOneDrop.bind(this);
+    }
+
+    renderOneDrop(dropPart) {
+        // {"partName":"Boltor Prime Stock",
+        // "drops":[
+        //      {"relicName":"R1","relicTier":"Axi","relicReference":"A-R1-U"},
+        //      {"relicName":"S3","relicTier":"Axi","relicReference":"A-S3-U"}
+        //  ]
+        // }
+
+        var dropText = dropPart.drops.map((ele) => ele.relicReference).join(' | ');
+        var text = dropPart.partName + ": " + dropText;
+
+        return <span style={{ border: "1px solid black", marginRight: "0.5ex", background: "white", color: "black", whiteSpace: "nowrap", display: "inline-block" }}>{text}</span>;
     }
 
     render() {
-        return <div>{JSON.stringify(this.state.partDrops)}</div>;
+        var drops = this.state.partDrops;
+
+        var items = drops.map(this.renderOneDrop);
+
+        //return <div>{JSON.stringify(this.state.partDrops)}<br/>{items}</div>;
+        return <div>{items}</div>;
     }
 }
